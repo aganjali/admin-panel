b2583bcc7636648b62874c0329e7d79ece5695c8
AmirHosein Rajabi <amirhosein.rajabi.ah7@gmail.com>
2025-07-16 23:33:27 +0330

    feat(#7): Integrate API - users page.

--- src/lib/api/users.ts
+++ src/lib/api/users.ts
@@ -1,10 +1,41 @@
-import { GetUsersInput, PagedResultDtoOfUserListDto } from "@/types";
+import {
+  ApiResponse,
+  ApiServicesAppUserDeleteuserDeleteParams,
+  ApiServicesAppUserGetuserforeditGetParams,
+  ApiServicesAppUserGetusersGetParams,
+  CreateOrUpdateUserInput,
+  GetUserForEditOutput,
+  PagedResultDtoOfUserListDto,
+} from "@/types";
 import { http } from "../http";
 
 export const usersApi = {
-  getUsers: (data: GetUsersInput) =>
-    http.post<PagedResultDtoOfUserListDto, GetUsersInput>({
+  getUsers: (params: ApiServicesAppUserGetusersGetParams) =>
+    http.get<
+      ApiResponse<PagedResultDtoOfUserListDto>,
+      ApiServicesAppUserGetusersGetParams
+    >({
       url: "/api/services/app/User/GetUsers",
+      params,
+    }),
+  getUserForEdit: (params: ApiServicesAppUserGetuserforeditGetParams) =>
+    http.get<
+      ApiResponse<GetUserForEditOutput>,
+      ApiServicesAppUserGetuserforeditGetParams
+    >({
+      url: "/api/services/app/User/GetUserForEdit",
+      params,
+    }),
+  createOrUpdateUser: (data: CreateOrUpdateUserInput) =>
+    http.post<ApiResponse<void>, CreateOrUpdateUserInput>({
+      url: "/api/services/app/User/CreateOrUpdateUser",
       data,
     }),
+  deleteUser: (params: ApiServicesAppUserDeleteuserDeleteParams) =>
+    http.del<ApiResponse<void>, void, ApiServicesAppUserDeleteuserDeleteParams>(
+      {
+        url: "/api/services/app/User/DeleteUser",
+        params,
+      }
+    ),
 };
